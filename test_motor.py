ea621cd07fe08eb7bd0a38ba5c36c75d2c1bf916
ehrntomi99 <thomasehrnleitner@gmail.com>
2026-03-20 17:43:24 +0100

    Add two very simple tests

--- test_motor.py
+++ test_motor.py
@@ -1,11 +1,20 @@
 import asynchronous_motor
 
-
+def test_torques_values():
+    """ Test if function returns M_max and M_n """
+    result = asynchronous_motor.torques(200000, 100000, 5000)
+    assert len(result) == 2
+    
 def test_torques():
     """ Check if torque calculation is correct for 200kW, 100kW """
     M_n, M_max = asynchronous_motor.torques(200000, 100000, 5000)
     assert round(M_max, 2) == 381.97
     assert round(M_n, 2) == 190.99
+
+def test_torques_15000_values():
+    """ Test if function returns two values """
+    result = asynchronous_motor.torques_15000(150, 300)
+    assert len(result) == 2
 
 def test_torques_15000():
     """ Test torque at speed of 15000 rpm """

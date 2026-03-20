6d8b6bb40e7d88e78d7235cc30c81d188e373f66
ehrntomi99 <thomasehrnleitner@gmail.com>
2026-03-20 16:44:33 +0100

    Swap M_n, M_max

--- test_motor.py
+++ test_motor.py
@@ -1,6 +1,6 @@
 import asynchronous_motor
 
 def test_torques():
-    M_max, M_n = asynchronous_motor.torques(200000, 100000, 5000)
+    M_n, M_max = asynchronous_motor.torques(200000, 100000, 5000)
     assert round(M_max, 2) == 381.97
     assert round(M_n, 2) == 190.99

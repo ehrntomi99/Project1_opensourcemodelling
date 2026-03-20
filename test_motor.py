4053ee62f9c45d2062ad5b117767a49d4e0c1146
ehrntomi99 <thomasehrnleitner@gmail.com>
2026-03-20 17:15:14 +0100

    Second test
    test_torques_15000

--- test_motor.py
+++ test_motor.py
@@ -2,6 +2,12 @@ import asynchronous_motor
 
 
 def test_torques():
+    """ Check if torque calculation is correct for 200kW, 100kW """
     M_n, M_max = asynchronous_motor.torques(200000, 100000, 5000)
     assert round(M_max, 2) == 381.97
     assert round(M_n, 2) == 190.99
+
+def test_torques_15000():
+    """ Test torque at speed of 15000 rpm """
+    M_n_15, M_max_15 = asynchronous_motor.torques_15000(300, 150, 5000, 15000)
+    assert round(M_max_15, 0) == 100
